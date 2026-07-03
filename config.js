145d26f368d73344c66f9b49733e68896a4dbd52
thiennhatgroup <thiennhatgroup@users.noreply.github.com>
2026-07-03 07:35:11 +0000

    deploy: 022ba2179cbe8e47d018d634adef204837c7bb7c

--- config.js
+++ config.js
@@ -15,4 +15,6 @@
 window.APP_CONFIG = {
   supabaseUrl: "https://nsxvasvceslhhvgjkedh.supabase.co",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zeHZhc3ZjZXNsaGh2Z2prZWRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4NzY2MDgsImV4cCI6MjA5ODQ1MjYwOH0.PuGwDEVHu3n0V4X5uhx-X04swUjQfjZLuhN5Ug6K924",
+  // VAPID PUBLIC key (an toàn để công khai). Private key CHỈ đặt trong Supabase secrets.
+  vapidPublicKey: "BLWLk086shDtwqT32D9zHcaB4vCc63aAiWMF4z0QfbicphUYX2YPLUYG6Dx1qZgqB7jh24JjuUUA00j5n5sNOqo",
 };
